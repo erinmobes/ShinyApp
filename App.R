ffa501fc1a583e86bc2a90510420fdd96210fdbc
Alisha <a.lad@wustl.edu>
2018-11-03 16:39:55 -0500

    commit

--- App.R
+++ App.R
@@ -5,51 +5,54 @@ library(shiny)
 library(rsconnect)
 library(shinydashboard)
 library(foreign)
+library(ggplot2)
+library(plotly)
+
 # blahhh
 ## bllaaahhh
-
-# Download World Bank data and turn into data.table
-myDT <- data.table(
-  wb(indicator = c(
-    "SP.POP.TOTL", # Population
-    #   "SP.DYN.LE00.IN", # Life expectancy at birth, total (years)
-    "CC.EST", # Control of Corruption: Estimate
-    # Refugees
-    #   "SM.POP.REFG",	# Refugee population by country or territory of asylum
-    #   "SM.POP.REFG.OR",	# Refugee population by country or territory of origin
-    #  "6.0.GDP_current", # GDP Current US Dollars
-    #   "6.0.GDPpc_constant", #GDP per capita, PPP (constant 2011 international $
-    "NY.GDP.MKTP.KD", #GDP (constant 2010 US$)
-    "GB.XPD.DEFN.GDP.ZS", # Defense expenditure (% of GDP)
-    "MS.MIL.MPRT.KD",	# Arms imports (SIPRI trend indicator values)
-    "MS.MIL.XPND.ZS",	# Military expenditure (% of central government expenditure)
-    "MS.MIL.XPND.GD.ZS"	# Military expenditure (% of GDP)
-  )
-  , mrv = 60)
-)
-# Download country mappings
-countries <- data.table(wbcountries())
-# Set keys to join the data sets
-setkey(myDT, iso2c)
-setkey(countries, iso2c)
-# Add regions to the data set, but remove aggregates
-myDT <- countries[myDT][ ! region %in% "Aggregates"]
-# Reshape data into a wide format
-wDT <- reshape(
-  myDT[, list(
-    country, region, date, value, indicator)],
-  v.names = "value",
-  idvar=c("date", "country", "region"),
-  timevar="indicator", direction = "wide")
-# Turn date, here year, from character into integer
-wDT[, date := as.integer(date)]
-setnames(wDT, names(wDT),
-         c("Country", "Region",
-           "Year",  "Population",
-           "Trust in Gov","GDP",
-           "Def Exp % GDP","Arms Imports",
-           "Mil Exp % GDP", "Mil Exp % Gov Exp"
-         ))
+# 
+# # Download World Bank data and turn into data.table
+# myDT <- data.table(
+#   wb(indicator = c(
+#     "SP.POP.TOTL", # Population
+#     #   "SP.DYN.LE00.IN", # Life expectancy at birth, total (years)
+#     "CC.EST", # Control of Corruption: Estimate
+#     # Refugees
+#     #   "SM.POP.REFG",	# Refugee population by country or territory of asylum
+#     #   "SM.POP.REFG.OR",	# Refugee population by country or territory of origin
+#     #  "6.0.GDP_current", # GDP Current US Dollars
+#     #   "6.0.GDPpc_constant", #GDP per capita, PPP (constant 2011 international $
+#     "NY.GDP.MKTP.KD", #GDP (constant 2010 US$)
+#     "GB.XPD.DEFN.GDP.ZS", # Defense expenditure (% of GDP)
+#     "MS.MIL.MPRT.KD",	# Arms imports (SIPRI trend indicator values)
+#     "MS.MIL.XPND.ZS",	# Military expenditure (% of central government expenditure)
+#     "MS.MIL.XPND.GD.ZS"	# Military expenditure (% of GDP)
+#   )
+#   , mrv = 60)
+# )
+# # Download country mappings
+# countries <- data.table(wbcountries())
+# # Set keys to join the data sets
+# setkey(myDT, iso2c)
+# setkey(countries, iso2c)
+# # Add regions to the data set, but remove aggregates
+# myDT <- countries[myDT][ ! region %in% "Aggregates"]
+# # Reshape data into a wide format
+# wDT <- reshape(
+#   myDT[, list(
+#     country, region, date, value, indicator)],
+#   v.names = "value",
+#   idvar=c("date", "country", "region"),
+#   timevar="indicator", direction = "wide")
+# # Turn date, here year, from character into integer
+# wDT[, date := as.integer(date)]
+# setnames(wDT, names(wDT),
+#          c("Country", "Region",
+#            "Year",  "Population",
+#            "Trust in Gov","GDP",
+#            "Def Exp % GDP","Arms Imports",
+#            "Mil Exp % GDP", "Mil Exp % Gov Exp"
+#          ))
 
 test <- source(file = "C://Users//Shilpa//Documents//R//shinyapp//GrossExportsPartnerShare.R")
 
@@ -59,17 +62,38 @@ ui <- dashboardPage(
   dashboardBody(
     fluidRow(
       box(
-        selectInput("dropdown", "Export Country of Comparison", choices = c("Vietnam", "Australia", "Thailand"))
+        selectInput("dropdown", "Export Country of Comparison", choices = dat$`Country Name`, selected =""),
+        plotlyOutput("graph1")
         )
     )
   )
 )
 
 server <- function (input, output, session){
-  dropdownInput <- reactive({
-    
-  })
+  
+  
+  output$graph1 <- renderPlotly(
+    ggplot(dat[dat$`Country Name`== input$dropdown,], aes(x=Year, y=value, country = iso2, group = Partner, stroke = "black")) + 
+      geom_line() + geom_flag(data = first_last_mid_point[first_last_mid_point$`Country Name` == input$dropdown,], 
+                              size = 10, show.legend = T) + 
+      theme_classic() +
+      theme(legend.position = "top", panel.background = element_rect(fill = "lightgrey"), plot.background = element_rect(fill = "lightgrey"), legend.background = element_rect(fill = "lightgrey"))  + 
+      labs(title = paste0(input$dropdown, ": Share of Gross Exports by Country"), y = "% of Gross Exports", country = "") + 
+      scale_country(labels=c("China", "Russia",  "United States"))
+  )
+# 
+#     output$graph1 <- renderPlotly(
+#       plot_ly(dat[dat$`Country Name`== input$dropdown,], aes(x=Year, y=value, country = iso2, group = Partner, stroke = "black")) + 
+#         geom_line() + geom_flag(data = first_last_mid_point[first_last_mid_point$`Country Name` == input$dropdown,], 
+#         size = 10, show.legend = T) + 
+#         theme_classic() +
+#         theme(legend.position = "top", panel.background = element_rect(fill = "lightgrey"), plot.background = element_rect(fill = "lightgrey"), legend.background = element_rect(fill = "lightgrey"))  + 
+#         labs(title = paste0(input$dropdown, ": Share of Gross Exports by Country"), y = "% of Gross Exports", country = "") + 
+#         scale_country(labels=c("China", "Russia",  "United States"))
+#               )
 }
+
+
 # 
 # ui = shinyUI(fluidPage(
 #   titlePanel("Analysis Test"),
@@ -92,33 +116,33 @@ server <- function (input, output, session){
 #                    plotOutput("phonePlot")  
 #                    
 #                  )))))))
-
-server = shinyServer(function(input, output, session) {
-  
-  output$motionchart2 <- renderGvis({
-    gvisMotionChart(wDT, idvar = "Country",
-                    timevar = "Year",
-                    xvar = "Mil Exp % Gov Exp",
-                    yvar = "Trust in Gov",
-                    sizevar = "GDP",
-                    colorvar = "Region")
-  })
-  
-  output$phonePlot <- renderPlot({
-    
-    # Render a barplot
-    barplot(WorldPhones[,input$region]*1000, 
-            main=input$region,
-            ylab="Number of Telephones",
-            xlab="Year")
-  })
-  
-})
-
-barplot(WorldPhones[,'Oceania']*1000, 
-        main='Oceania',
-        ylab="Number of Telephones",
-        xlab="Year")
+# 
+# server = shinyServer(function(input, output, session) {
+#   
+#   output$motionchart2 <- renderGvis({
+#     gvisMotionChart(wDT, idvar = "Country",
+#                     timevar = "Year",
+#                     xvar = "Mil Exp % Gov Exp",
+#                     yvar = "Trust in Gov",
+#                     sizevar = "GDP",
+#                     colorvar = "Region")
+#   })
+#   
+#   output$phonePlot <- renderPlot({
+#     
+#     # Render a barplot
+#     barplot(WorldPhones[,input$region]*1000, 
+#             main=input$region,
+#             ylab="Number of Telephones",
+#             xlab="Year")
+#   })
+#   
+# })
+# 
+# barplot(WorldPhones[,'Oceania']*1000, 
+#         main='Oceania',
+#         ylab="Number of Telephones",
+#         xlab="Year")
 
 # setwd('C:/Users/1274806318A/Desktop/App')
 
